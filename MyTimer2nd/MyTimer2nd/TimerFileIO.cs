422ed4389c1cf09d45551a052aca7d0cefaff3ee
ishikura <ishikura@yk.rim.or.jp>
2013-01-11 16:40:05 +0900

    fixed #9

---
+++ MyTimer2nd/MyTimer2nd/TimerFileIO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace WpfApplication1
+{
+    class TimerFileIo
+    {
+        const string inputValueFileName  = "c:\\TimerValue.txt";
+        const string outputValueFileName = "c:\\TimerValue.txt";// "c:\\OutTimerValue.txt";
+
+        public static IEnumerable<TimeSpan> ReadTimerValue()
+        {
+            if (File.Exists(inputValueFileName))
+            {
+                try
+                {
+                    return File.ReadLines(inputValueFileName).Select(c => TimeSpan.Parse(c));
+                }
+                catch(Exception e)
+                {
+                    // Formatエラーくらいしか想定してないが…。
+                    return new List<TimeSpan>() { new TimeSpan(0, 3, 0) };
+                }
+            }
+            else
+            {
+                // ファイルが無いときは1分だけ設定。
+                return new List<TimeSpan>() { new TimeSpan(0, 0, 10) };
+            }
+        }
+
+        public static void SaveTimerValue(List<TimeSpan> items)
+        {
+            if( items.Count == 0 )
+            {
+                return;
+            }
+
+            File.WriteAllLines(outputValueFileName, items.Select(c => c.ToString(@"hh\:mm\:ss")));
+        }
+    }
+}
